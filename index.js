a63544758c75f28a9073c5e3b368658df5f93974
Kevinho12gs <129360050+Kevinho12gs@users.noreply.github.com>
2025-01-08 21:56:57 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css'; // El archivo de estilos
+import LoginApp from './LoginApp';
+
+ReactDOM.render(<LoginApp />, document.getElementById('root'));
